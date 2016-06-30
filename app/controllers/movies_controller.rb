02e0e3a0dcba32eb8f165c11e657dd8018b22f89
Snk <snknikolov@gmail.com>
2016-06-30 17:10:34 +0000

    Ready for Heroku

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,42 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+  
+  def show
+      id = params[:id] # movie id from URI route
+      @movie = Movie.find(id)
+  end
+  
+  def new
+      @movie = Movie.new
+  end
+  
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :release_date)
+  end
+
+  def edit
+      @movie = Movie.find params[:id]
+  end
+  
+  def update
+      @movie = Movie.find params[:id]
+      @movie.update_attributes!(movie_params)
+      flash[:notice] = "#{@movie.title} was successfully updated."
+      redirect_to movie_path(@movie)
+  end
+  
+  def create
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path  
+  end
+  
+  def destroy
+      @movie = Movie.find(params[:id])
+      @movie.destroy
+      flash[:notice] = "Movie #{@movie.title} deleted."
+      redirect_to movies_path
+  end
+end
